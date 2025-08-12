e5b76508378a1070f5acdc6f54b693b478fa5f50
Yash Chaudhari <choudhariyash7890@gmail.com>
2025-08-12 11:44:54 +0530

    new update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,21 @@
 "use client";
-import type { Metadata } from "next";
+
 import "./globals.css";
 import Header from "./components/Header";
 import { usePopupToggle } from "./hooks/usePopupToggle";
 import FormPopup from "./components/utils/FormPopup";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   const { isOpen, openPopup, closePopup } = usePopupToggle();
+
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/png" href="/unpluginfinity-favicon.png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100..900;1,100..900&family=Oswald:wght@200..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
