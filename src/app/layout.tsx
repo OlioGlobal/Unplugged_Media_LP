8fa634d9c0ea5bfdccf7e059118d2653f7953f92
Yash Chaudhari <choudhariyash7890@gmail.com>
2025-08-13 10:02:39 +0530

    gtm tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,8 +20,27 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100..900;1,100..900&family=Oswald:wght@200..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TXFX8RJP');
+            `,
+          }}
+        />
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TXFX8RJP"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Header openPopup={openPopup} />
         <FormPopup isOpen={isOpen} onClose={closePopup} />
         {children}
